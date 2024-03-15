2c2c213b693b33e8e5c6b820da9e52c70cdb359b
lsvvt <losev.tim.v@gmail.com>
2024-03-15 17:22:40 +0300

    update R plots

--- Figures/Fig_Ar/R.R
+++ Figures/Fig_Ar/R.R
@@ -5,50 +5,28 @@ library(dplyr)
 library(reshape2)
 
 Ar2_tab <- read.csv("Ar2_Fx_Fxc.csv", sep=",", header=T) %>% select(-X)
-p <- Ar2_tab %>%
-  left_join(data.frame(Name=c("PBE", "r2SCAN", "M06-2X", "piM06-2X", "piM06-2X-DL", "M06-2X-Ω100"), lw=c(2,1,1,1,1,1)*0.5)) %>%
+Ar2_plot <- Ar2_tab %>%
+  filter(!Name%in%c("r2SCAN")) %>% 
   mutate(Type=factor(Type, levels=c("Fx","Fxc"),labels=c("F[x]","F[xc]")),
-         Name=factor(Name, levels=c("PBE", "r2SCAN", "M06-2X", "piM06-2X", "piM06-2X-DL", "M06-2X-Ω100"))) %>%
-  ggplot(aes(x=z, y=Value, color=Name, linewidth=lw)) +
-    geom_line() +
+         Name=factor(Name, levels=c("r2SCAN", "M06-2X", "piM06-2X", "piM06-2X-DL", "M06-2X-Ω100", "PBE"))) %>%
+  ggplot() +
+    geom_line(aes(x=z, y=Value, color=Name, linewidth=Name, linetype=Name),
+              alpha=0.7) +
     coord_cartesian(ylim=c(0,3), xlim=c(-7,7), expand=F) +
     scale_x_continuous("Position, Angstrom", breaks=c(seq(-6,6,1.5)), minor_breaks=seq(-7,7,0.5)) +
     scale_y_continuous("Enhancement factor") +
-    scale_color_manual("Functional", values=c("forestgreen", "black","deeppink", "darkorange1", "dodgerblue", "yellow"),
+    scale_color_manual("Functional", values=c("black", "#e41a1c","deeppink", "darkorange1", "dodgerblue", "green"),
                                      breaks=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL", "M06-2X-Ω100"),
                                      labels=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL", "M06-2X-Ω100")) +
-    scale_linewidth_identity() +
+    scale_linetype_manual("Functional", values=c("solid", "solid", "solid", "solid", "solid", "solid"),
+                     breaks=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL", "M06-2X-Ω100"),
+                     labels=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL", "M06-2X-Ω100")) +
+    scale_linewidth_manual("Functional", values=c(0.7, 1.2, 1.2, 1.2, 1.2, 1.2),
+                        breaks=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL", "M06-2X-Ω100"),
+                        labels=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL", "M06-2X-Ω100")) +
     facet_wrap(~Type, labeller = label_parsed) +
     theme_bw() +
     theme(legend.position="bottom") +
-    guides(linewidth="none", color="legend")
-
-ggsave("FigAr2.png", p, units="cm", dpi=1000, width=18, height=10)
-
-Ar2_tab_baseline <- Ar2_tab %>%
-  filter(Name == "PBE") %>%
-  select(z, Value, Type) %>%
-  rename("baseline" = "Value")
-
-p <- Ar2_tab %>%
-  left_join(Ar2_tab_baseline, by=c("z", "Type")) %>%
-  mutate(diff = Value - baseline) %>%
-  select(-Value, -baseline) %>%
-  left_join(data.frame(Name=c("PBE", "r2SCAN", "M06-2X", "piM06-2X", "piM06-2X-DL", "M06-2X-Ω100"), lw=c(2,1,1,1,1,1)*0.5)) %>%
-  mutate(Type=factor(Type, levels=c("Fx","Fxc"),labels=c("F[x]","F[xc]")),
-         Name=factor(Name, levels=c("PBE", "r2SCAN", "M06-2X", "piM06-2X", "piM06-2X-DL", "M06-2X-Ω100"))) %>%
-  ggplot(aes(x=z, y=diff, color=Name, linewidth=lw)) +
-    geom_line() +
-    coord_cartesian(ylim=c(-1,2), xlim=c(-7,7), expand=F) +
-    scale_x_continuous("Position, Angstrom", breaks=c(seq(-6,6,1.5)), minor_breaks=seq(-7,7,0.5)) +
-    scale_y_continuous(expression("EF - EF"^"PBE")) +
-    scale_color_manual("Functional", values=c("forestgreen", "black","deeppink", "darkorange1", "dodgerblue", "yellow"),
-                                     breaks=c("PBE", "r2SCAN","M06-2X","piM06-2X","piM06-2X-DL", "M06-2X-Ω100"),
-                                     labels=c("PBE", "r2SCAN","M06-2X","piM06-2X","piM06-2X-DL", "M06-2X-Ω100")) +
-    scale_linewidth_identity() +
-    facet_wrap(~Type, labeller = label_parsed) +
-    theme_bw() +
-    theme(legend.position="bottom") +
-    guides(linewidth="none", color="legend")
-
-ggsave("FigAr2-baseline.png", p, units="cm", dpi=1000, width=18, height=10)
+    guides(linewidth="legend", color="legend")
+Ar2_plot
+ggsave("FigAr2.png", Ar2_plot, units="cm", dpi=1000, width=18, height=10)
--- Figures/Fig_EnhFactor/R.R
+++ Figures/Fig_EnhFactor/R.R
@@ -3,6 +3,7 @@
 library(ggplot2)
 library(dplyr)
 library(reshape2)
+library(tidyverse)
 
 # Functional colors
 #[1] "#E41A1C" "#377EB8" "#4DAF4A" "#984EA3" "#FF7F00" "#A65628"
@@ -13,53 +14,69 @@ library(reshape2)
 # piM06-2X    "darkorange"
 # piM06-2X-DL "dodgerblue"
 
-Fx_Fxc_tab <- read.csv("Fx-Fxc.csv", sep=";", header=T)
+Fx_Fxc_tab <- read.csv("Fx-Fxc.csv", sep=";", header=T) %>% 
+  filter(s > 0, !Name%in%c("R2SCAN")) %>%
+  mutate(alpha=factor(as.character(as.integer(alpha)), levels=c("1000000000","1","0")),
+    var=factor(var, levels=c("Fx","Fxc"),labels=c("F[x]","F[xc]")),
+    Name=factor(Name, levels=c("R2SCAN", "M06-2X", "piM06-2X", "piM06-2X-DL", "PBE")))
 
-p <- Fx_Fxc_tab %>%
-  filter(s > 0) %>%
-  left_join(data.frame(Name=c("PBE", "R2SCAN", "M06-2X", "piM06-2X", "piM06-2X-DL"), lw=c(2,1,1,1,1))) %>%
-  mutate(alpha=factor(as.character(as.integer(alpha)), levels=c("0","1","1000000000"), labels=c("0", "1", "10^9")),
-         var=factor(var, levels=c("Fx","Fxc"),labels=c("F[x]","F[xc]")),
-         Name=factor(Name, levels=c("PBE", "R2SCAN", "M06-2X", "piM06-2X", "piM06-2X-DL"))) %>%
-  ggplot(aes(x=s, y=val, linetype=alpha, color=Name, linewidth=lw)) +
-    geom_hline(yintercept=1.804, color="blue", linewidth=0.8) +
-    geom_line(alpha=1) +
-    coord_cartesian(ylim=c(-0.2,3.3), xlim=c(0,5), expand=F) +
-    scale_x_continuous(expression(paste("Normed gradient, ", italic(s)))) +
-    scale_y_continuous("Enhancement factor", breaks=seq(-5,5,1)) +
-    facet_wrap(~var, labeller = label_parsed) +
-    scale_color_manual("Functional", values=c("forestgreen", "black","deeppink", "darkorange1", "dodgerblue"),
-                                     breaks=c("PBE","R2SCAN","M06-2X","piM06-2X","piM06-2X-DL"),
-                                     labels=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL")) +
-    scale_linetype_manual(expression(alpha), labels = c("0", "1", expression(10^9)), values = c("solid", "dashed", "dotted")) +
-    scale_linewidth_identity() +
-    geom_hline(yintercept=0, color="gray80", linewidth=0.8) +
-    theme_bw() +
-    theme(legend.position="bottom") +
-    guides(linewidth="none", color="legend", linetype="legend")
+Fx_Fxc_plot <- Fx_Fxc_tab %>%
+  ggplot() +
+  geom_hline(yintercept=0, color="gray80", linewidth=0.8) +
+  geom_hline(yintercept=1.804, color="grey60", linewidth=0.8) +
+  geom_text(label="Lieb-Oxford bound", x=2.5, y=1.9, color="grey60",
+            inherit.aes = F) +
+  geom_line(aes(x=s, y=val, linetype=alpha, linewidth=Name, color=Name), 
+            alpha=0.7) +
+  coord_cartesian(ylim=c(-0.2,3.3), xlim=c(0,5), expand=F) +
+  scale_x_continuous(expression(paste("Normed gradient, ", italic(s)))) +
+  scale_y_continuous("Enhancement factor", breaks=seq(-5,5,1)) +
+  facet_wrap(~var, labeller = label_parsed) +
+  scale_color_manual("Functional", 
+                     values=c("black", "#e41a1c","deeppink", "darkorange1", "dodgerblue"),
+                     #values=c("#4daf4a", "#e41a1c","#984ea3", "darkorange1", "dodgerblue"),
+                     breaks=c("PBE","R2SCAN","M06-2X","piM06-2X","piM06-2X-DL"),
+                     labels=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL")) +
+  scale_linewidth_manual("Functional", 
+                     values=c(0.7, 1.2, 1.2, 1.2, 1.2),
+                     breaks=c("PBE","R2SCAN","M06-2X","piM06-2X","piM06-2X-DL"),
+                     labels=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL")) +
+  scale_linetype_manual(expression(alpha), labels = c("0", "1", expression(10^9)), 
+                        breaks=c("0","1","1000000000"),
+                        values = c("solid", "44", "22")) +
+  theme_bw() +
+  theme(legend.position="bottom") +
+  guides(linetype="legend")
+Fx_Fxc_plot
+ggsave("Fig_fx_fxc-clipped.png", Fx_Fxc_plot, units="cm", dpi=1000, width=19, height=10*19/18)
 
-ggsave("Fig_fx_fxc-cutted.png", p, units="cm", dpi=1000, width=19, height=10*19/18)
-
-p <- Fx_Fxc_tab %>%
-  filter(s > 0) %>%
-  left_join(data.frame(Name=c("PBE", "R2SCAN", "M06-2X", "piM06-2X", "piM06-2X-DL"), lw=c(2,1,1,1,1))) %>%
-  mutate(alpha=factor(as.character(as.integer(alpha)), levels=c("0","1","1000000000"), labels=c("0", "1", "10^9")),
-         var=factor(var, levels=c("Fx","Fxc"),labels=c("F[x]","F[xc]")),
-         Name=factor(Name, levels=c("PBE", "R2SCAN", "M06-2X", "piM06-2X", "piM06-2X-DL"))) %>%
-  ggplot(aes(x=s, y=val, linetype=alpha, color=Name, linewidth=lw)) +
-    geom_line(alpha=1) +
-    coord_cartesian(ylim=c(-4,5), xlim=c(0,5), expand=F) +
-    scale_x_continuous(expression(paste("Normed gradient, ", italic(s)))) +
-    scale_y_continuous("Enhancement factor", breaks=seq(-5,5,1)) +
-    facet_wrap(~var, labeller = label_parsed) +
-    scale_color_manual("Functional", values=c("forestgreen", "black","deeppink", "darkorange1", "dodgerblue"),
-                                     breaks=c("PBE","R2SCAN","M06-2X","piM06-2X","piM06-2X-DL"),
-                                     labels=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL")) +
-    scale_linetype_manual(expression(alpha), labels = c("0", "1", expression(10^9)), values = c("solid", "dashed", "dotted")) +
-    scale_linewidth_identity() +
-    geom_hline(yintercept=0, color="gray80", linewidth=0.8) +
-    theme_bw() +
-    theme(legend.position="bottom") +
-    guides(linewidth="none", color="legend", linetype="legend")
-
-ggsave("Fig_fx_fxc-full.png", p, units="cm", dpi=1000, width=19, height=10*19/18)
+Fx_Fxc_plot_SI <- Fx_Fxc_tab %>%
+  ggplot() +
+  geom_hline(yintercept=0, color="gray80", linewidth=0.8) +
+  geom_hline(yintercept=1.804, color="grey60", linewidth=0.8) +
+  geom_text(label="Lieb-Oxford bound", x=2.5, y=2.1, color="grey60",
+            inherit.aes = F) +
+  geom_line(aes(x=s, y=val, linetype=alpha, linewidth=Name, color=Name), 
+            alpha=0.7) +
+  coord_cartesian(ylim=c(-4,5), xlim=c(0,5), expand=F) +
+  scale_x_continuous(expression(paste("Normed gradient, ", italic(s)))) +
+  scale_y_continuous("Enhancement factor", breaks=seq(-5,5,1)) +
+  facet_wrap(~var, labeller = label_parsed) +
+  scale_color_manual("Functional", 
+                     values=c("black", "#e41a1c","deeppink", "darkorange1", "dodgerblue"),
+                     #values=c("#4daf4a", "#e41a1c","#984ea3", "darkorange1", "dodgerblue"),
+                     breaks=c("PBE","R2SCAN","M06-2X","piM06-2X","piM06-2X-DL"),
+                     labels=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL")) +
+  scale_linewidth_manual("Functional", 
+                         values=c(0.7, 1.2, 1.2, 1.2, 1.2),
+                         breaks=c("PBE","R2SCAN","M06-2X","piM06-2X","piM06-2X-DL"),
+                         labels=c("PBE","r2SCAN","M06-2X","piM06-2X","piM06-2X-DL")) +
+  scale_linetype_manual(expression(alpha), 
+                        breaks=c("0","1","1000000000"),
+                        labels = c("0", "1", expression(10^9)), 
+                        values = c("solid", "44", "22")) +
+  theme_bw() +
+  theme(legend.position="bottom") +
+  guides(linetype="legend")
+Fx_Fxc_plot_SI
+ggsave("Fig_fx_fxc-SI.png", Fx_Fxc_plot_SI, units="cm", dpi=1000, width=19, height=10*19/18)
